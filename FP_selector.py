6bc0fb65b50320cd4bddb391dac0245b02c68c82
jack-thomson-smith <jackthomsonsmith31@gmail.com>
2025-01-17 12:46:23 -0800

    proof of concept for accessing FPBase API

--- FP_selector.py
+++ FP_selector.py
@@ -1,5 +1,15 @@
 import requests
+import csv
 
-response = requests.get("https://www.fpbase.org/api/proteins/")
-print(response.status_code)
-print(response.json())  
+r = requests.get("https://www.fpbase.org/api/proteins/?name__icontains=green&default_state__qy__gte=0.7")
+print(r.status_code)
+print(dir(r))
+
+f = open("placeholder_csv.csv", "w")  # if dne, creates it, otherwise nothing
+f.write(r.text)
+f.close()
+
+f = open("placeholder_csv.csv", "r")
+reader = csv.DictReader(f)
+for i in reader:
+    print(i["name"])
