c78e106ee7108d2e73ff11b9d21a1e27a49e477d
jack-thomson-smith <jackthomsonsmith31@gmail.com>
2025-01-18 20:38:07 -0800

    added CSV file buffer, README.txt

--- FP_selector.py
+++ FP_selector.py
@@ -1,15 +1,32 @@
 import requests
 import csv
 
-r = requests.get("https://www.fpbase.org/api/proteins/?name__icontains=green&default_state__qy__gte=0.7")
-print(r.status_code)
-print(dir(r))
+"""
+FPBase API documentation: https://www.fpbase.org/api/
+Requests Module documentation: https://requests.readthedocs.io/en/
+"""
 
-f = open("placeholder_csv.csv", "w")  # if dne, creates it, otherwise nothing
-f.write(r.text)
-f.close()
+url = "https://www.fpbase.org/api/proteins/"
 
-f = open("placeholder_csv.csv", "r")
-reader = csv.DictReader(f)
-for i in reader:
-    print(i["name"])
+payload = {}   # "field__lookup":"value"
+
+field = "name"
+lookup = "icontains"
+value = "green"
+payload[field + "__" + lookup] = value
+
+r = requests.get(url, params=payload)
+print("\nStatus:", r.status_code)
+print("Lookup url:", r.url, "\n")
+
+csv_file = open("csv_buffer.csv", "w")
+csv_file.write(r.text)
+csv_file.close()
+
+csv_file = open("csv_buffer.csv", "r")
+reader = csv.DictReader(csv_file)
+for row in reader:
+    for key in row.keys():
+        print(key)
+
+csv_file.close()
---
+++ README.txt
@@ -0,0 +1,14 @@
+Welcome to FP Selector! 
+This application chooses the best fluorescent proteins for you to use
+based off of spectral overlap, quantum yield, size, and other factors.
+It's coded in python and uses the FPBase REST API to get data.
+
+PLAN:
+
+modules to use: Tkinter, Requests, MatplotLib, 
+You pick how many proteins you want.
+You pick further critera (qy, price, etc.)
+
+the program generates you the proteins it thinks fit your specifications.
+the program generates links to the papers behind the proteins, and to the 
+proteins' graphs on FPPicker (ex: https://www.fpbase.org/protein/mazamigreen/).
