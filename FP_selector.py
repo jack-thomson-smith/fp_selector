7d37911aa0c89bc34480458b20b6161b5a44f1b3
jack-thomson-smith <jackthomsonsmith31@gmail.com>
2025-01-31 15:18:42 -0800

    added almost complete compatibility picking functionality

--- FP_selector.py
+++ FP_selector.py
@@ -1,15 +1,15 @@
 import requests
 import csv
-import operator as o
 # import tkinter as tk
 from time import sleep
 
 """
 FPBase API documentation: https://www.fpbase.org/api/
 Requests Module documentation: https://requests.readthedocs.io/en/
-"""
+Additional Sources:
+https://blog.addgene.org/choosing-your-fluorescent-proteins-for-multi-color-imaging
 
-PWD = 50  # protein wavelength distribution
+"""
 
 
 def refresh_database(field="default_state__em_max", lookup="gte", value="400"):
@@ -19,50 +19,55 @@ def refresh_database(field="default_state__em_max", lookup="gte", value="400"):
     should return all proteins in the database.
 
     Arguments:
-        field: a string of the field you want to look up (ex: name, seq,
-                default_state_qy)
-        lookup: a string of the lookup operator you want to use (ex: lt,
-                contains)
-        value: a string of the value you want to find (ex: green, 541)"""
+        field: a string of the field you want to look up
+               (default: default_state__em_max)
+        lookup: a string of the lookup operator you want to use
+               (default: gte)
+        value: a string of the value you want to find
+               (default: 400)"""
 
     url = "https://www.fpbase.org/api/proteins/"
     payload = {f"{field}__{lookup}": value}  # "field__lookup":"value"
-    response_object = requests.get(url, params=payload)
-    print(f"status: {response_object.status_code} ({response_object.reason})")
-    with open("_protein_database.csv", "w") as csv_file:
-        csv_file.write(response_object.text)
-        #  writes refreshed data to file
+    response_obj = requests.get(url, params=payload)
 
-    order_by_brightness(create_protein_list)
+    print(f"fetch status: {response_obj.status_code} ({response_obj.reason})")
+
+    with open("_protein_database.csv", "w", encoding="utf-8") as csv_file:
+        csv_file.write(response_obj.text)
+
+    order_by_brightness(build_protein_list())
+    # ^ orders protein list by brightness (bright->dark) & writes info to file
 
 
-def create_protein_list(file="_protein_database.csv"):
-    """ reads _protein_database.csv and outputs a list of dicts.
+def build_protein_list(file="_protein_database.csv"):
+    """ reads csv file and outputs a list of dicts.
 
     Arguments:
-        file: a csv file (like _protein_database.csv).
+        file: a csv file (default: _protein_database.csv).
 
     Returns: a list of dicts of protein data. Each dict is a row in
     the file and is mapped to the uuid of the protein."""
 
     output = []
-    with open(file, "r") as csv_file:
+    with open(file, "r", encoding="utf-8") as csv_file:
         for line in csv.DictReader(csv_file):
             output.append(line)
     return output
 
 
 def order_by_brightness(protein_list, file="_fp_ordered_brightness.csv"):
-    """ WIP! need to develop sorting algorithm to rank proteins by brightness,
-    then write that info to file.
+    """ Sorts protein list, then writes info in csv format to a file.
 
+    Arguments:
+        protein_list: list of dict of proteins
+        file: file to write to (default: _fp_ordered_brightness.csv)
     """
     sorted_list = sorted(protein_list,
                          key=lambda x: x["states.0.brightness"],
                          reverse=True)
     # ^ sorts list by brightness, highest -> lowest
 
-    with open(file, "w", newline='') as list_file:
+    with open(file, "w", newline="", encoding="utf-8") as list_file:
         fieldnames = sorted_list[0].keys()
         writer = csv.DictWriter(list_file, fieldnames=fieldnames)
 
@@ -70,58 +75,124 @@ def order_by_brightness(protein_list, file="_fp_ordered_brightness.csv"):
         writer.writerows(sorted_list)
 
 
-def maximize_for_brightness(protein_list):
-    """ Given list of protein dicts, chooses brightest one, and
-    returns its emission max, excitation max, brightness value, and uuid.
-    Could make this function more efficient by not accessing pdict values
-    for every increment of brightness, and instead only accessing it once
-    the loop has found the brightest one. Might do that someday!
+def build_allowed_spectrum(constraint, radius):
+    """function description tbd"""
+    constraints_spec = set(range(constraint-(radius), constraint+(radius)+1))
+    return set(range(400, 701)) - constraints_spec
+
+
+def choose_compatible(sorted_protein_list, constraints):
+    """Chooses compatible proteins based off of constraints parameter.
+    The idea is to find the brightest proteins that have both em_maxes
+    and ex_maxes that are at least 60-100 nm seperated.
 
     Arguments:
-        protein_list: dict of dicts from create_protein_dict()
-    Returns: (em max of protein, ex max, brightness value, uuid) """
+        sorted_protein_list: list of proteins (presorted by brightness).
+        constraints: dict in the format {"em_max": int,
+                                         "ex_max": int,
+                                         "order": int}
+                    "order" denotes which protein will be chosen. If it's
+                    1, then the first protein that matches the criteria
+                    will be chosen. If it's 2, then the second protein that
+                    matches the criteria will be chosen, and so on.
 
-    output = (0, 0, 0, None)  # (em_max, ex_max, brightness, uuid)
-    for pdict in protein_list.values():
-        if output[2] < pdict["states.0.brightness"]:
+    Returns: protein dict, or None if no protein was found."""
 
-            output = (pdict["states.0.em_max"],
-                      pdict["states.0.ex_max"],
-                      pdict["states.0.brightness"],
-                      pdict["uuid"])
-    return output
+    output_index = None
+    r = 110
+
+    while output_index is None:
+        r -= 10  # every loop, decreases radius until a protein is found\
+        print("current radius:", r)
+        allowed_em_spec = build_allowed_spectrum(constraints["em_max"], r)
+
+        allowed_ex_spec = build_allowed_spectrum(constraints["ex_max"], r)
+        order = constraints["order"] + 1
+        print("order:", order)
+        for index, protein in enumerate(sorted_protein_list):
+            allowed_em = int(protein["states.0.em_max"]) in allowed_em_spec
+            allowed_ex = int(protein["states.0.ex_max"]) in allowed_ex_spec
+
+            if allowed_em and allowed_ex:
+                order -= 1  # every loop, decreases the order until it's 1
+                if order == 1:
+                    output_index = index
+
+        if r <= 55:  # radius is too low, so protein can't be found w/o overlap
+            print("radius is too low...")
+            return output_index
+
+    return sorted_protein_list[output_index]
+
+
+def output_compatible(sorted_protein_list, name, order):
+    """function description tbd"""
+
+    p_info = None
+    for p in sorted_protein_list:
+        if p["name"].casefold() == name.casefold():
+            p_info = (int(p["states.0.em_max"]), int(p["states.0.ex_max"]))
+    # ^ case-insensitive input mapping to protein
+    if p_info is None:
+        return "invalid name"
+
+    brightest_p_dict = choose_compatible(sorted_protein_list,
+                                         {'em_max': p_info[0],
+                                          'ex_max': p_info[1],
+                                          'order': order})
+    return brightest_p_dict
 
 
 if __name__ == "__main__":
+
+    ordered_plist = build_protein_list("_fp_ordered_brightness.csv")
+    command = ""
+    commands = ["list by brightness (lbb)", "pick compatible proteins (pcp)",
+                "refresh database (rd)", "list commands (ls)", "exit"]
+
     print("")
     print("Welcome to FP_Selector!")
     print("")
-
-    command = ""
-    print("\nPlease enter command:")
+    print("")
     while command != "exit":
+        print("\nPlease enter command:")
         command = input("> ")
-        if command == "list dict":
-            for (key, value) in protein_list.items():
-                print(f"{key}: {value}\n\n")
-        elif command == "list names":
-            count = 1
-            for protein in protein_list.values():
-                print(f"{count}: {protein['name']}")
-                count += 1
-                sleep(0.01)
+
+        if command == "list by brightest" or command == "lbb":
+            for i in range(10):
+                print(f"{i+1}: {ordered_plist[i]['name']}")
+                sleep(0.05)
+
         elif command == "pick compatible proteins" or command == "pcp":
-            num = input("how many?\n> ")
-            if num == 2:
-                prompt_choose_compatible(protein_list)
-            else:
-                print("functionality has not been added.")
-        elif command == "refresh database":
+            name = input("Please enter protein name below.\n   > ")
+
+            for i in range(10):
+                brightest_p = output_compatible(ordered_plist, name, i+1)
+
+                if brightest_p == "invalid name":
+                    print("Invalid protein name.")
+                    break
+                elif brightest_p is None:
+                    print("No more proteins can be found.")
+                    break
+
+                print(f"{i}: {brightest_p['name']}")
+                print(f"    brightness: {brightest_p['states.0.brightness']}")
+                print(f"    emission max: {brightest_p['states.0.em_max']}")
+                print(f"    excitation max: {brightest_p['states.0.ex_max']}")
+
+        elif command == "refresh database" or command == "rd":
             refresh_database()
-        elif command == "else":
-            pass  # loop breaks due to command now being "exit"
+
+        elif command == "list commands" or command == "ls":
+            print("commands:")
+            for c in commands:
+                print(" ", c)
+
         else:
-            print("unknown")
+            if command != "exit":
+                print("unknown")
+
     print("")
     print("_______")
     print("Thanks for using FP_Selector!")
